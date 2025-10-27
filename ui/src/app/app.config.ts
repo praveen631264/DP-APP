e144a2a3ee969ea7ea711ad6e9c25dee13a71294
praveen631264 <praveen631264@gmail.com>
2025-10-27 04:16:17 +0000

    minor refactoring

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideAnimations(),
+    provideHttpClient(),
+    importProvidersFrom(NgxChartsModule)
+  ]
 };
