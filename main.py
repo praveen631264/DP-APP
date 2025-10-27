fb9fdabbdf8fcd712956bf62f89f3eb5e49703a5
praveen631264 <praveen631264@gmail.com>
2025-10-27 02:56:49 +0000

    minor refactoring

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 from app import create_app
 
-app, celery = create_app()
-
-if __name__ == '__main__':
-    # In a real production environment, you would use a production-ready WSGI server
-    # like Gunicorn or uWSGI instead of the Flask development server.
-    # Example with Gunicorn: gunicorn --worker-class eventlet -w 1 main:app
-    from app.realtime_events import socketio
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True, use_reloader=False)
+app = create_app()
+celery = app.celery
