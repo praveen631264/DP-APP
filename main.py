7f332819b3ca39318d850c3004be6155b03f1afd
praveen631264 <praveen631264@gmail.com>
2025-10-28 19:12:06 +0000

    minor refactoring

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from app import create_app
-from app.celery_worker import celery
 
+from app import create_app
+from app.celery_worker import make_celery
+
+# Create the Flask app instance
 app = create_app()
+
+# Use the app context to configure and create the Celery instance
+celery = make_celery(app)
