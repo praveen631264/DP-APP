26a5831501cd7ed4e9debdfea5f8a7a90418c01b
praveen631264 <praveen631264@gmail.com>
2025-10-29 14:58:18 +0000

    minor refactoring

---
+++ app/utils/json_encoder.py
@@ -0,0 +1,20 @@
+from flask.json.provider import JSONProvider
+import json
+from bson import ObjectId
+import datetime
+
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime.datetime):
+            return o.isoformat()
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+class CustomJSONProvider(JSONProvider):
+    def dumps(self, obj, **kwargs):
+        return json.dumps(obj, **kwargs, cls=CustomJSONEncoder)
+
+    def loads(self, s, **kwargs):
+        return json.loads(s, **kwargs)
+
