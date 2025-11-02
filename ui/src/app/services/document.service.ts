e92d3cea592d22ba5cabce28a70c5c0fe4c6f781
praveen631264 <praveen631264@gmail.com>
2025-11-02 18:59:35 +0000

    minor refactoring

--- ui/src/app/services/document.service.ts
+++ ui/src/app/services/document.service.ts
@@ -1,6 +1,6 @@
 
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Document } from '../models/document.model';
 import { environment } from '../../environments/environment';
@@ -15,30 +15,31 @@ export interface PaginatedDocumentsResponse {
 })
 export class DocumentService {
   private apiUrl = `${environment.apiUrl}/documents`;
+  private headers = new HttpHeaders({ 'X-Proxy-To': 'flask-app' });
 
   constructor(private http: HttpClient) { }
 
   getDocuments(params: any): Observable<PaginatedDocumentsResponse> {
-    return this.http.get<PaginatedDocumentsResponse>(this.apiUrl, { params });
+    return this.http.get<PaginatedDocumentsResponse>(this.apiUrl, { params, headers: this.headers });
   }
 
   getDocument(docId: string): Observable<Document> {
-    return this.http.get<Document>(`${this.apiUrl}/${docId}`);
+    return this.http.get<Document>(`${this.apiUrl}/${docId}`, { headers: this.headers });
   }
 
   uploadDocument(formData: FormData): Observable<any> {
     return this.http.post(this.apiUrl, formData, {
       reportProgress: true,
-      observe: 'events'
+      observe: 'events',
+      headers: this.headers
     });
   }
 
   deleteDocument(docId: string): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/${docId}`);
+    return this.http.delete(`${this.apiUrl}/${docId}`, { headers: this.headers });
   }
 
   updateDocumentKvps(docId: string, kvps: { [key: string]: any }, version: number): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${docId}/kvp`, { kvps, _version: version });
+    return this.http.put(`${this.apiUrl}/${docId}/kvp`, { kvps, _version: version }, { headers: this.headers });
   }
-
 }
